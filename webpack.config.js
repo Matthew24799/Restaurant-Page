27205de9b1014df1ce0ebf07a62a6153634c0c3a
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-09-21 20:42:07 -0400

    created html template., added plugins for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -8,4 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
