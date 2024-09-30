cc86605fe4ab62cb23ed256e516ea77cec4de6e8
Matthew Rogers <142711667+squidthescrub@users.noreply.github.com>
2024-09-29 23:39:55 -0700

    added a header and some text content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log("HEY");
+const content = document.getElementById("content");
+
+const header = document.createTextNode("MAC AND CHEESE BITCHES")
+const p = document.createTextNode("WELCOME to the mac and cheese restraunt!!! take a look around at our menu")
+
+content.appendChild(header);
+content.appendChild(p);
