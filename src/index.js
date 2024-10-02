fda3000d41af8c4008be4b9bb7c162386e6f27e9
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-10-02 02:02:28 -0400

    styled home page content

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const headerDiv = document.createElement("div")
 headerDiv.setAttribute("class", "headerDiv")
 headerDiv.backgroundColor 
 const headerText = document.createTextNode("MAC & CHEESE RESAURANT");
-const p = document.createElement("p")
-p.textContent = "we have been making mac n cheese for many generations we got a wide variety to choose from!" 
+const p = document.createElement("div")
+p.textContent = "Mac & Cheese Emporium, a whimsical restaurant that has been delighting cheese lovers since its grand opening in 1987. Founded by culinary enthusiast Clara Beechwood, who grew up in a small town with a legendary family recipe, the Emporium was born from a desire to elevate the humble mac and cheese into a gourmet experience. Clara's passion for comfort food and creativity led her to experiment with artisanal cheeses and unique toppings, transforming traditional dishes into mouthwatering masterpieces. Over the years, the restaurant has become a local favorite, known for its cozy atmosphere and inventive flavors, like Truffle Bacon Bliss and Spicy Jalapeño Mac. Today, the Mac & Cheese Emporium celebrates its rich history while continually innovating, proving that comfort food can be both nostalgic and adventurous." 
 p.setAttribute("class", "p")
 
 
