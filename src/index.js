89b9e4fd33d7a57c7e305299efc3bae2878c534b
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-10-02 01:42:26 -0400

    added classes to elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-console.log("HEY");
+import "./style.css"
+const header = document.getElementById("header");
 const content = document.getElementById("content");
 const headerDiv = document.createElement("div")
+headerDiv.setAttribute("class", "headerDiv")
 headerDiv.backgroundColor 
-const header = document.createTextNode("MAC AND CHEESE RESAURANT");
-const p = document.createTextNode("we have been making mac n cheese for many generations we got a wide variety to choose from!")
+const headerText = document.createTextNode("MAC & CHEESE RESAURANT");
+const p = document.createElement("p")
+p.textContent = "we have been making mac n cheese for many generations we got a wide variety to choose from!" 
+p.setAttribute("class", "p")
 
-content.appendChild(headerDiv)
-headerDiv.appendChild(header);
+
+
+header.appendChild(headerDiv)
+headerDiv.appendChild(headerText);
 content.appendChild(p);
