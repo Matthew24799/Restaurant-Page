621a751bfcb7d2fe95766577a59801f3649daa10
Matthew Rogers <matthewrogers24799@gmail.com>
2024-11-22 23:25:25 -0800

    added a menu picture

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,9 @@
 import "./style.css"
-
+import sandwich from "./macSandwich.jpg"
 
 const food1 = document.createElement("img");
-
+food1.src = sandwich
 const food2 = document.createElement("img");
-
 const food3 = document.createElement("img");
+
+document.appendChild(food1);
